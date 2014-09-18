7b8e72a3dc9e6017df92f1cffe54f297c3598311
Arushi Vashist <arushi.vashist@students.iiit.ac.in>
2014-09-19 03:34:43 +0530

    alert latitude and longitude

---
+++ source/Week 6/geocode/app/assets/javascripts/application.js
@@ -0,0 +1,54 @@
+var geocoder = new google.maps.Geocoder();
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = { 
+    zoom: 8,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+      alert("Latitude: "+latitude +"Longitude: "+longitude);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+
+      });
+
+      
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }   
+  }); 
+}
+
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
