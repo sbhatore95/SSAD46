1809808250f413b4165ab9986c195feafd313dca
Siddharth Bhatore <sbhatore95@gmail.com>
2014-09-25 22:56:42 +0530

    Merged Source and tests - source => geocode(Week 6), tests => google_api/demo4.html(Week7) and tests => auth(Week 8)

---
+++ source/Week 8/SCFT/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+	    end
+def new
+ 	@user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+	 redirect_to root_url, :notice => "Signed up!"
+    else
+         render "new"
+    end
+  end
+end
