c6f51696072e8fc71f6708df357ed33146a4ac10
Arushi Vashist <arushi.vashist@students.iiit.ac.in>
2014-10-09 20:22:20 +0530

    query

--- source/Week 8/SCFT/app/controllers/search_controller.rb
+++ source/Week 8/SCFT/app/controllers/search_controller.rb
@@ -1,7 +1,11 @@
 class SearchController < ApplicationController
   def printvals
   	@cityname= params[:city]
+	@lat= params[:latitude_range]
+	@ele= params[:elevation_range]
+	@rad= params[:radius_range]
+	@results = Location.where(City: params[:city])
 #  	render :text => cityname
-	render :text => @cityname.inspect
+#	render :text => @results.inspect
   end
 end
--- source/Week 8/SCFT/app/controllers/welcome_controller.rb
+++ source/Week 8/SCFT/app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+	   @results = Location.where(City: params[:city])
+	   render: none
+
   end
 end
