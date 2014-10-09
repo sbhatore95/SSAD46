0417c81eb6f8bf71c8474b006e31f4b469efd8de
Arushi Vashist <arushi.vashist@students.iiit.ac.in>
2014-10-09 18:39:26 +0530

    css

--- source/Week 8/SCFT/app/controllers/search_controller.rb
+++ source/Week 8/SCFT/app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def printvals
-  	cityname= params[:city]
-  	render :text => cityname
+  	@cityname= params[:city]
+#  	render :text => cityname
+	render :text => @cityname.inspect
   end
 end
