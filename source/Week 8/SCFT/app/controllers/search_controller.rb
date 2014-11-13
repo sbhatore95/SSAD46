732509fdae6b50eb76d1dd83767c5140d8a2b3fa
Siddharth Bhatore <sbhatore95@gmail.com>
2014-11-13 13:09:15 +0530

    Showing results for all including the cities with epw files present

--- source/Week 8/SCFT/app/controllers/search_controller.rb
+++ source/Week 8/SCFT/app/controllers/search_controller.rb
@@ -13,40 +13,32 @@ class SearchController < ApplicationController
 	$ilongitude_mycity = 0
 	$ielevation_mycity = 1000
 	def getsearchdata
-
-		#$icityname= params[:city]
-#		$ilatitude_range= params[:latitude_range]
-#		$ielevation_range= params[:elevation_range]
-#		$iradius_range= params[:radius_range]
-
-		#$ilatitude_range= params[:latitude_range]
-		#$ielevation_range= params[:elevation_range]
-		#$iradius_range= params[:radius_range]
-
-
-
-		@mycity = Location.find_by(City: $icityname)
-
-		#If the city does not exists in database 
-		if @mycity == nil
 	
 			redirect_to :action => 'findSurrogateCity' ,:city => params[:city] ,:latitude_range => params[:latitude_range], :elevation_range => params[:elevation_range] ,:radius_range => params[:radius_range]
 			
-			#If the city  exist in database 
-		else
-			redirect_to :action => 'exists', :city => params[:city]
-
-		end 
-
 	end
 
 	def findSurrogateCity
 
-		if $found == 0
+
+		
 			$icityname= params[:city]
 			$ilatitude_range= params[:latitude_range]
 			$ielevation_range= params[:elevation_range]
 			$iradius_range= params[:radius_range]
+
+		
+		if Cities.exists?(city: $icityname)
+ 	
+		 	#redirect_to :action => 'weather' ,:city => params[:city] ,:latitude_range => params[:lat], :elevation_range => params[:altrange] ,:radius => params[:radrange]
+			$found = 1
+		#If the city  exist in database 
+		
+		else
+			#redirect_to :action => 'exists', :city => params[:city]
+			$found = 0
+
+		end 
 
 
 		#get data from JS code of geocoder
@@ -55,6 +47,7 @@ class SearchController < ApplicationController
 			@city = $icityname
 			@lat_range = $ilatitude_range
 			@rad_range = $iradius_range
+			@elev_range = $ielevation_range
 	
 		#set range
 			@latitude_Range_Start = $ilatitude_mycity.to_f - $ilatitude_range.to_f
@@ -65,14 +58,7 @@ class SearchController < ApplicationController
 			@elevation_Range_Start = $elevation_mycity.to_f - $ielevation_range.to_f
 			@elevation_Range_End = $elevation_mycity.to_f + $ielevation_range.to_f
 
-			@results = Location.where('Latitude > ? AND Latitude < ? AND Elevation > ? AND Elevation < ?',@latitude_Range_Start,@latitude_Range_End,@elevation_Range_Start, @elevation_Range_End)
-
-		else
-		
-			redirect_to :action => 'exists', :city => $icityname		
-
-		end
-
+			@results = Cities.where('latitude > ? AND latitude < ? AND elevation > ? AND elevation < ?',@latitude_Range_Start,@latitude_Range_End,@elevation_Range_Start, @elevation_Range_End)
 
 	end
 
@@ -84,36 +70,19 @@ class SearchController < ApplicationController
 		$ilatitude_mycity = params[:latitude_mycity]
 		$ilongitude_mycity = params[:longitude_mycity]
 
-		
-	    @mycity = Location.find_by(City: $icityname)
+	end
 
-		#If the city does not exists in database 
-		if @mycity == nil
- 	
-		 	#redirect_to :action => 'weather' ,:city => params[:city] ,:latitude_range => params[:lat], :elevation_range => params[:altrange] ,:radius => params[:radrange]
-			$found = 0
-		#If the city  exist in database 
-		
-		else
-			#redirect_to :action => 'exists', :city => params[:city]
-			$found = 1
-
-		end 
- 
-
-
-	end 
-
+=begin
 	def exists
-		@ecity = Location.find_by(City: params[:city])
-		@cityname = @ecity.City
-		@citycountry = @ecity.Country
-		@cityelev = @ecity.Elevation
+		@ecity = Cities.find_by(city: params[:city])
+		@cityname = @ecity.city
+		@citycountry = @ecity.country
+		@cityelev = @ecity.elevation
 
 		
 
 	end
-
+=end
 end
 			
 			
