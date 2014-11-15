798864825490a39baa2eaeaf3ead3a1d92152888
Arushi Vashist <arushi.vashist@students.iiit.ac.in>
2014-11-16 00:06:57 +0530

    download thing

--- source/Week 8/SCFT/app/controllers/surrogate_controller.rb
+++ source/Week 8/SCFT/app/controllers/surrogate_controller.rb
@@ -1,4 +1,10 @@
 class SurrogateController < ApplicationController
+	@EPW_PATH = File.join(Rails.root, "public", "EpwFiles")
 	def displaySurrogateInfo
 	end
+	
+	def download 		
+ 		send_file "#{Rails.root}/public/EpwFiles/EGYPT.epw"
+	end
 end
+
--- source/Week 8/SCFT/config/routes.rb
+++ source/Week 8/SCFT/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'search/weather'
   get 'search/weather' => 'search#weather' , :via => 'post'
   get '/search/exists' => 'search#exists'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -26,8 +26,8 @@ get 'sessions/new'
 get 'users/new'
 get 'search/new'
 get 'surrogate/displaySurrogateInfo'
-
-
+get 'surrogate/download'
+post 'surrogate/download' => "surrogate#download" 
 
 #resources :location 
 
