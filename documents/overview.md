02714b870515b56c6b1afa5a680a1616e9ba3a63
= <=>
2024-06-09 12:23:03 +0530

    ITER1: implement maps api mark city

--- documents/overview.md
+++ documents/overview.md
@@ -6,8 +6,14 @@
 1. Google Maps API City Marker
     - make a google cloud project (SCFT) (done),
     - lat and long dataset (countries and states) (done),
+2. POC(1)
 
 
+```shell
+# POC(1)
+python3 manage.py migrate
+python3 manage.py makemigrations
+```
 ## 6 June 2024
 
 Progress:
--- source/CityFinder/CFMaps/views.py
+++ source/CityFinder/CFMaps/views.py
@@ -7,6 +7,13 @@ class CFMaps:
   def __init__(self):
     pass
   
+  def getLatLong(self, cityname):
+     dictLL = {}
+     city1 = City.objects.get(cityname=cityname)
+     dictLL['lat'] = city1.lat
+     dictLL['lng'] = city1.long
+     return dictLL
+  
   def parse_csv(self):
     if flag:
         f = open('./static/files/cities.csv', 'r')
@@ -39,8 +46,8 @@ def get_params(request, keys):
   return params
 
 
-def check_empty(params):
-  return ((params["cityname"] == None) or (params["cityname"] == ""))
+def check_empty(params, key):
+  return ((params[key] == None) or (params[key] == ""))
 
 
 def index(request):
@@ -49,8 +56,10 @@ def index(request):
     cfmap1.parse_csv()
     params = get_params(request, ['cityname', 'lat_range', 'alt'])
     context = {}
-    if check_empty(params):
+    latlng = {}
+    if check_empty(params, 'cityname'):
         pass
     else:
-        context = {'data': params}
-    return render(request, 'CFMaps/index.html', context)
+        latlng = cfmap1.getLatLong(params['cityname'])
+        context = {'data': params, 'latlng': latlng}
+    return render(request, 'CFMaps/index.html', {})
